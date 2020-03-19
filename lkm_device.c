b3053146b97036a425186183b999e922cb101134
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-03-19 17:15:43 +0100

    Adding skeleton functions for prototypes and structure for file operations

--- lkm_device.c
+++ lkm_device.c
@@ -9,6 +9,54 @@ MODULE_AUTHOR("Thomas Piekarski");
 MODULE_DESCRIPTION("A Sandbox device module for the Linux Kernel");
 MODULE_VERSION("0.1");
 
+// Prototypes
+static int device_open(struct inode*, struct file*);
+static int device_release(struct inode*, struct file*);
+static ssize_t device_read(struct file*, char*, size_t, loff_t*);
+static ssize_t device_write(struct file*, const char*, size_t, loff_t*);
+
+// Definitions
+#define DEVICE_NAME "lkm_device"
+#define MESSAGE "Hello, Linux!\n"
+#define MESSAGE_BUFFER_LENGTH 15
+
+// Statics
+static int major_num;
+static int device_open_count = 0;
+static char message_buffer[MESSAGE_BUFFER_LENGTH];
+static char* message_ptr;
+
+static struct file_operations file_ops = {
+    .open = device_open,
+    .read = device_read,
+    .release = device_release,
+    .write = device_write
+};
+
+static ssize_t device_read(struct file* flip, char* buffer, size_t len, loff_t* offset) {
+    // todo: implement reading operation
+
+    return 0;
+}
+
+static ssize_t device_write(struct file* flip, const char* buffer, size_t len, loff_t *offset) {
+    // todo: implement writing operation
+
+    return 0;
+}
+
+static int device_open(struct inode* inode, struct file* file) {
+    // todo: implement device opening
+
+    return 0;
+}
+
+static int device_release(struct inode* inode, struct file* file) {
+    // todo: implement device releasing
+
+    return 0;
+}
+
 static int __init lkm_device_init(void) {
     printk(KERN_INFO "Initialize Sandbox Device Module...\n");
 

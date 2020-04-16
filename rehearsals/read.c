0314e8a4005b1b8cdb817ec7dd194a46be927d7b
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-04-16 10:18:33 +0200

    Rehearse low-level reading of files

---
+++ rehearsals/read.c
@@ -0,0 +1,37 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FILE_PERMISSION "r"
+
+void read(FILE *fp)
+{
+	int ch = fgetc(fp);
+	while (!feof(fp)) {
+		putc(ch, stdout); // possible shortcut: putchar(ch);
+		ch = fgetc(fp);
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 2) {
+		printf("usage: %s <filename>\n", *argv);
+
+		exit(-EINVAL);
+	}
+
+	const char *filename = argv[1];
+	FILE *fp = fopen(*(++argv), FILE_PERMISSION);
+
+	if (fp == NULL) {
+		printf("Failed opening file %s, aborting.\n", filename);
+
+		exit(-ENOENT);
+	}
+
+	read(fp);
+	fclose(fp);
+
+	exit(0);
+}

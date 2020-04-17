c1db6c2f3e098f7934ee911448680216d6173c62
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-04-17 12:20:02 +0200

    Trying to add cdev and creating setup function

--- lkm_mev.c
+++ lkm_mev.c
@@ -21,6 +21,7 @@
  * 
  */
 
+#include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -47,15 +48,44 @@ static ssize_t mev_write(struct file *file, const char __user *buf,
 static dev_t mev_device;
 static struct file_operations mev_fops = {
 	.owner = THIS_MODULE,
-	.llseek = mev_llseek,
-	.read = mev_read,
-	.write = mev_write,
+
+	// todo: implement dummies for prototypes and uncomment
+	//.llseek = mev_llseek,
+	//.read = mev_read,
+	//.write = mev_write,
 	// Using new ioctl and avoiding BKL (Big Kernel Lock)
 	// For details refer to: https://lwn.net/Articles/119652/
-	.unlocked_ioctl = mev_ioctl,
-	.open = mev_open,
-	.release = mev_release
+	//.unlocked_ioctl = mev_ioctl,
+	//.open = mev_open,
+	//.release = mev_release
 };
+
+struct mev_dev {
+	// todo: declare all members by working through subsequent chapters
+	struct cdev cdev;
+};
+
+static struct mev_dev *dev;
+
+static int lkm_mev_setup_cdev(struct mev_dev *dev, int index)
+{
+	int dev_no = MKDEV(MAJOR(mev_device), MINOR(mev_device) + index);
+
+	cdev_init(&dev->cdev, &mev_fops);
+	dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &mev_fops;
+
+	int rc = cdev_add(&dev->cdev, dev_no, 1);
+
+	if (rc < 0) {
+		printk(KERN_ERR "%s: Failed adding cdev, %d, %d.\n",
+		       THIS_MODULE->name, MAJOR(dev_no), MINOR(dev_no));
+
+		return rc;
+	}
+
+	return 0;
+}
 
 static int __init lkm_mev_init(void)
 {
@@ -70,6 +100,21 @@ static int __init lkm_mev_init(void)
 		return rc;
 	}
 
+	// todo: WIP, at the moment kernel panic due to dereferencing a null pointer
+	// (Propably because callback pointers in fops are not implemented)
+
+	/* 
+
+	rc = lkm_mev_setup_cdev(dev, 0);
+
+	if (rc < 0) {
+		printk(KERN_ERR "%s: Failed setting up cdev.\n",
+		       THIS_MODULE->name);
+
+		return rc;
+	}
+	*/
+
 	return 0;
 }
 
@@ -77,6 +122,8 @@ static void __exit lkm_mev_exit(void)
 {
 	if (mev_device != 0) {
 		unregister_chrdev_region(mev_device, LKM_MEV_DEVICE_COUNT);
+
+		cdev_del(&dev->cdev);
 	}
 }
 

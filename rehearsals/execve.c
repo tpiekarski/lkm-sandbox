10114ac89d3aefaab9dfb5c8f724ea28dd6e42a1
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-06-08 11:57:29 +0200

    Adding a target executable for testing with execve

--- rehearsals/execve.c
+++ rehearsals/execve.c
@@ -2,17 +2,20 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define EXECVE_TARGET "execve-target"
+
 int main(int argc, char *argv[], char *envp[])
 {
-	printf("Executing another program with execve()...\n");
-	int rcval = execve("fork", argv, envp);
+	printf("Running execve PID=%d\n", getpid());
+	printf("Executing %s with execve\n", EXECVE_TARGET);
+	int rcval = execve(EXECVE_TARGET, argv, envp);
 
 	// current process will be left here if execve was successful
 
 	if (rcval < 0)
 		goto out;
 
-	printf("Still here... exiting program.\n");
+	printf("This line will never be hit!\n");
 
 	return 0;
 
